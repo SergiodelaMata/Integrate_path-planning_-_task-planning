0e3b5a8bda2b4a35cceec35bc582b9725955520a
SergiodelaMata <sergio.matam@edu.uah.es>
2020-05-22 00:47:25 +0200

    Added the file to introduce the theta star algorithm.

---
+++ r2p2/r2p2/thetaStar.py
@@ -0,0 +1,31 @@
+""" This module implements Theta*'s path planning algorithm.
+
+Two variants are included: grid-based, and mesh-based.
+
+"""
+
+__author__ = "Sergio de la Mata Moratilla; Javier Pastor Moreno"
+__authors__ = ["Sergio de la Mata Moratilla; Javier Pastor Moreno"]
+__contact__ = "sergio.matam@edu.uah.es; javier.pastor@edu.uah.es"
+__copyright__ = "Copyright 2020, UAH"
+__credits__ = ["Mario Cobos Maestre"]
+__date__ = "2020/05/22"
+__deprecated__ = False
+__email__ =  "sergio.matam@edu.uah.es; javier.pastor@edu.uah.es"
+__license__ = "GPLv3"
+__maintainer__ = "Sergio de la Mata Moratilla; Javier Pastor Moreno"
+__status__ = "Development"
+__version__ = "0.0.1"
+
+"""
+    Code modified from https://https://github.com/ISG-UAH/R2P2
+"""
+
+
+def thetaStar():
+
+    return 0
+
+def thetaStar_mesh():
+
+    return 0
